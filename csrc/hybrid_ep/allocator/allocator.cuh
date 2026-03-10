dfd27db024a477f335743910b366c013fdf8b762
liyuhang <yuhang.li@aminer.cn>
2026-03-10 11:56:51 +0800

    remove unsed header

--- csrc/hybrid_ep/allocator/allocator.cuh
+++ csrc/hybrid_ep/allocator/allocator.cuh
@@ -3,7 +3,7 @@
 
 #pragma once
 #include <cuda.h>
-#include <cuda_profiler_api.h>
+//#include <cuda_profiler_api.h>
 #include <cuda_runtime.h>
 #include <cuda_runtime_api.h>
 #include <torch/torch.h>
